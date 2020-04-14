81512dbd3596a68c8e5fbb0e65cd0b26c4e0fb86
kvezal <muckam49@mail.ru>
2020-04-14 18:01:33 +0800

    feat(template): search template added (module3-task2)

--- src/express/routes/search/search-route.js
+++ src/express/routes/search/search-route.js
@@ -2,8 +2,22 @@
 
 const {Router} = require(`express`);
 
+const mock = require(`../../../../mock`);
+const {adaptData} = require(`../../utils`);
+
 const searchRouter = new Router();
 
-searchRouter.get(`/`, (req, res) => res.send(`/search`));
+searchRouter.get(`/`, (req, res) => {
+    const ticketList = adaptData(mock);
+    res.render(`search/search`, {
+        title: `search`,
+        description: `Страница поиска`,
+        popularTicketListParams: {
+            hiddenTitle: `Самые обсуждаемые предложения`,
+            title: `Самые обсуждаемые`,
+            ticketList,
+        },
+    });
+});
 
 module.exports = searchRouter;
